7ed80dc47a4c1ec0f0e4b3106000af3ba38d092e
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-06-14 21:22:55 +0300

    style: change code style
    Update Container-1.js

--- React-Bootstrap/Container-1.js
+++ React-Bootstrap/Container-1.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import './ContainerSample.scss';
 
 
@@ -7,42 +7,33 @@ import './ContainerSample.scss';
 function ContainerSample() {
   return (
     <div>
-      <Container>
-        <Row >
-          <Col className='custom-row'>some information in simple-Container</Col>
-          <Col className='custom-row'>some information in simple-Container</Col>
-        </Row>
-        <Row >
-          <Col className='custom-row'>another information in simple-Container</Col>
-          <Col className='custom-row'>central data block in simple-Container</Col>
-          <Col className='custom-row'>another information in simple-Container</Col>
-        </Row>     
-      </Container>
       <Container fluid="md">
-        <Row >
+        <Row className="mt-4">
           <Col className="bg-info border">some information in fluid-Container</Col>
           <Col className="bg-info border">some information in fluid-Container</Col>
           </Row>
-        <Row >
+        <Row className="mb-4">
           <Col className="bg-info border">another information in fluid-Container</Col>
           <Col className="bg-info border">central data block in fluid-Container</Col>
           <Col className="bg-info border">another information in fluid-Container</Col>
         </Row>     
       </Container>
       <Container>
-        <Row >
-          <Col xs={12} md={9} xl={6} className='custom-row'>content: xs=12 md=9 xl=6</Col>
-          <Col xs={12} md={9} xl={6} className='custom-row'>content: xs=12 md=9 xl=6</Col>
-        </Row>
-        <Row className='my-4'>
-          <Col xs={11} md={8} xl={5} className='custom-row'>content: xs=11 md=8 xl=5</Col>
-          <Col xs={10} md={7} xl={4} className='custom-row'>content: xs=10 md=7 xl=4</Col>
-          <Col xs={9} md={6} xl={3} className='custom-row'>content: xs=9 md=6 xl=3</Col>
+        <Row>
+          <Col xs={10} md={7} xl={4} className='custom-col'>
+            content: xs=10 (83.33%), md=7 (58.33%), xl=4 (33.33%)
+          </Col>
+          <Col xs={{ span: 10, offset: 2 }} md={{ span: 7, offset: 2 }} xl={{ span: 4, offset: 2 }} className='custom-col'>
+            content: xs=10 md=7 xl=4 & offset=2
+          </Col>
+          <Col xs={9} md={6} xl={3} className='custom-col'>content: xs=9 md=6 xl=3</Col>
         </Row>   
         <Row>
-          <Col xs={8} md={5} xl={2} className='custom-row'>content: xs=8 md=5 xl=2</Col>
-          <Col xs={8} md={5} xl={2} className='custom-row'>content: xs=8 md=5 xl=2</Col>
-          <Col xs={8} md={5} xl={2} className='custom-row'>content: xs=8 md=5 xl=2</Col>
+          <Col xs={8} md={5} xl={2} className='custom-col'>content: xs=8 md=5 xl=2</Col>
+          <Col xs={8} md={5} xl={2} className='custom-col'>content: xs=8 md=5 xl=2</Col>
+          <Col xs={8} md={5} xl={2} className='custom-col'>content: xs=8 md=5 xl=2</Col>
+          <Col>
+        </Col>
         </Row>    
       </Container>
     </div>
