492824d57744c36ce4371c6876855f0c14e075d6
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-07-16 17:57:04 +0300

    refactor: optimize performance without changing overall behavior
    Update App.jsx

--- Material-UI/Vite-MUI/src/App.jsx
+++ Material-UI/Vite-MUI/src/App.jsx
@@ -1,38 +1,31 @@
 
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 
-import Header from './components/paperbase/Header';
-import Content from './components/paperbase/Content';
-import Navigator from './components/paperbase/Navigator';
-import Paperbase from './components/paperbase/Paperbase';
+import Boxes from './components/layout/Boxes';
+import Grids from './components/layout/Grids';
+
+
 
 import ProTip from './components/common/ProTip';
 import Copyright from './components/common/Copyright';
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="md" sx={{ border: '1px dashed grey' }}>
 
-      <Navigator />
+      <Typography variant="h4" component="h1" gutterBottom>
+        Vite React Material UI
+      </Typography>
 
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Vite React Material UI
-        </Typography>
-        <Header />
-        <Content />               
-      </Box>
+      <Boxes />
+      <Grids />
 
-      <Box sx={{ my: 4 }}>
-        <Paperbase />
-      </Box>
 
-      <Box sx={{ my: 4 }}>
-        <ProTip />
-        <Copyright />
-      </Box>  
+
+
+      <ProTip />
+      <Copyright />   
          
     </Container>
   );
